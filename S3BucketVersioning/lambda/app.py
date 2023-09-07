316cd1800b63dd28b561082a4aba95e0e0591494
Paul Brazell <pbraz@amazon.com>
2023-09-07 13:56:29 -0400

    Fixed bug in lambda function if bucket has no tags

--- S3BucketVersioning/lambda/app.py
+++ S3BucketVersioning/lambda/app.py
@@ -8,12 +8,15 @@ def lambda_handler(event, context) -> dict:
     s3 = boto3.client("s3")
 
     # If bucket has tag "DisableVersioning", do not enable versioning
-    tags = s3.get_bucket_tagging(Bucket=bucket_name)
-    if "DisableVersioning" in [tag["Key"] for tag in tags["TagSet"]]:
-        return {
-            "statusCode": 200,
-            "body": f"Bucket versioning is intentionally disabled for {bucket_name}. You can exclude this bucket from this check via the Trusted Advisor console",
-        }
+    try:
+        tags = s3.get_bucket_tagging(Bucket=bucket_name)
+        if "DisableVersioning" in [tag["Key"] for tag in tags["TagSet"]]:
+            return {
+                "statusCode": 200,
+                "body": f"Bucket versioning is intentionally disabled for {bucket_name}. You can exclude this bucket from this check via the Trusted Advisor console",
+            }
+    except Exception:
+        pass
 
     # Set bucket versioning to enabled
     try:
